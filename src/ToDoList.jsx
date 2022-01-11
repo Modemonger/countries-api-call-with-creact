14ed4c19a83c833daea1a0fe8258a0f49b197bb4
Modemonger <sta.modestas@gmail.com>
2022-01-11 22:23:19 +0200

    first try this time

---
+++ src/ToDoList.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ToDo from './ToDo';
+import style from './App.css'
+
+const ToDoList = ({toDoList, handleToggle, handleFilter}) => {
+    if(toDoList)
+        return (
+            <div className = {style.todos}>
+                {toDoList.map(todo => {
+                    return (
+                        <ToDo todo={todo} handleToggle={handleToggle} handleFilter={handleFilter}/>
+                    )
+                })}
+                <button onClick={handleFilter}>Clear Completed</button>
+            </div>
+        );
+    else
+        return (
+            <div className = {style.todos}>
+                
+            </div>
+        );
+};
+
+export default ToDoList;
